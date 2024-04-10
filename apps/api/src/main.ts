166fdb68b1feba2e5708248d77d55222ef3eeaaa
Chiemezie Jacob <Chiemezie.jacob@gmail.com>
2024-04-10 19:09:44 +0100

    Configured project to use Swagger API Documentation

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api')
+  app.setGlobalPrefix('api');
+  
+  const config = new DocumentBuilder()
+  .setTitle('NestJS Swagger Documentation')
+  .setDescription('API description')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagapi', app, document);
+  
   await app.listen(3000);
 }
 bootstrap();
